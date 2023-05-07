8cc3041ed8b2533994b5d747db44667403fb8219
Henning Hontheim <henning@hontheim.net>
2023-05-07 19:39:04 +0200

    push test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { SwPush } from "@angular/service-worker";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwa-test';
+  resp: string = '';
+
+  constructor(private _swPush: SwPush) {}
+
+  ngOnInit() {
+    this.requestSubscription();
+  }
+
+  requestSubscription = () => {
+    if (!this._swPush.isEnabled) {
+      console.log("Notification is not enabled.");
+      return;
+    }
+
+    this._swPush.requestSubscription({
+      serverPublicKey: 'BDkzqsc9wHMHb-gxoo7_dA0DDx39dxlUAqgM-TRkFQ8t_Kt9AH8s4fLqPyIK14cyZ_N6PL4mOb7CO_ztZgAOB10'
+    }).then((_) => {
+      console.log(JSON.stringify(_));
+      this.resp = JSON.stringify(_);
+    }).catch((_) => console.log);
+  };
+
 }
